6755e8f0cc3231521755591732b5b5b617a97e66
Hin Chi WONG <phantomcase@gmail.com>
2022-03-23 16:30:31 +0000

    Use setup reusable val

--- src/test/java/MarsCommanderTest.java
+++ src/test/java/MarsCommanderTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -5,10 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Mars Commander Test")
 class MarsCommanderTest {
+
+    MarsCommander marsCommander;
+
+    @BeforeEach
+    void setup(){
+        marsCommander = new MarsCommander();
+    }
+
     @Test
     @DisplayName("Assignment Test")
     void assignmentTest() {
-        MarsCommander marsCommander = new MarsCommander();
         assertEquals("1 3 N\n" +
                 "5 1 E",marsCommander.execute("5 5\n" +
                 "1 2 N\n" +
@@ -20,7 +28,6 @@ class MarsCommanderTest {
     @Test
     @DisplayName("UnValid Command Test")
     void unValidCommandTest(){
-        MarsCommander marsCommander = new MarsCommander();
         assertEquals("Not a Valid command!!!",marsCommander.execute("ABCDEFG"));
     }
 }
